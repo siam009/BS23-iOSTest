d07381d4016de00e3bae9ec7c76cb82eeb67f718
Arnab Ahamed Siam <siamarnab@gmail.com>
2024-02-15 21:06:00 +0600

    Initial Commit

---
+++ BS23-iOSTest/BS23_iOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BS23_iOSTestApp.swift
+//  BS23-iOSTest
+//
+//  Created by Arnab Ahamed Siam on 15/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BS23_iOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
